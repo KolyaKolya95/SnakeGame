3f6ba0f532188cfdcd5bc95fe5ad1fdbb9fbf67c
KolyaKolya95 <kolya.kolya1495@gmail.com>
2017-11-09 23:27:13 +0200

    message

---
+++ Snake/Snake.cpp
@@ -0,0 +1,131 @@
+// Snake.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+const int width = 20;
+const int height = 20;
+
+bool gameOver;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+
+eDirection dir;
+
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2 - 1;
+	y = height / 2 - 1;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+
+void Draw()
+{
+	system("cls");
+	for (int i = 0; i < width + 1; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++) 
+		{
+			if (j == 0 || j == width -1 )
+				cout << "#";
+			if (i == y && j == x)
+				cout << "0";
+			else if (i == fruitY && j == fruitX)
+				cout << "F"; 
+			else 
+				cout << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < width + 1; i++)
+		cout << "#";
+	cout << endl;
+	cout << "Score: " << score << endl;
+}
+
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
+}
+
+void Logic()
+{
+	switch (dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	}
+
+	if (x > width || x < 0 || y > height || y < 0)
+		gameOver = true;
+	if (x == fruitX && y == fruitY)
+	{
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+	}
+}
+
+int main()
+{
+	Setup();
+
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		Sleep(200);
+	}
+
+
+	cout << "Game Over";
+	getchar();
+
+	return 0;
+}
+
